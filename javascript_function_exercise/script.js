498933c058845d9a94371336a8f15188220634ca
sjpoot <78816393+sjpoot@users.noreply.github.com>
2021-03-12 10:05:21 +0100

    Add files via upload

---
+++ javascript_function_exercise/script.js
@@ -0,0 +1,8 @@
+const wallpaint = function (direction, color) {
+    console.log("The " + direction + " wall has been painted " + color);
+};
+
+/* wallpaint("green");
+wallpaint("yellow"); */
+wallpaint("north", "green");
+wallpaint("south", "yellow")
